fce21285d16f82c457c7c3e9a7cf63373ce8c342
Damian Obajtek <damian.obajtek@gmail.com>
2020-08-03 22:28:02 +0200

    basic component of cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
-import styles from "./App.module.css";
+import Styles from "./App.module.css";
 import { fetchData } from "./api";
 
 class App extends React.Component {
-  async componentDidMount() {
-    const data = await fetchData();
+  state = {
+    data: {},
+  };
 
-    console.log(data);
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchData });
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div className="container">
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
